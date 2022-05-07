6d42911191033fc3cc8311ca0751cb82d5634a0a
reneener <jhjin2u@gmail.com>
2022-05-07 17:46:43 +0900

    Spring JPA로 변경

--- src/main/java/com/reservation/reservationsystem/repository/StoreRepository.java
+++ src/main/java/com/reservation/reservationsystem/repository/StoreRepository.java
@@ -1,22 +1,10 @@
 package com.reservation.reservationsystem.repository;
 import com.reservation.reservationsystem.entity.store.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Repository
-public class StoreRepository {
-
-    @PersistenceContext
-    EntityManager em;
-
-    public void save(Store store){
-        em.persist(store);
-    }
-
-    public Store findByStoreId(Long id){
-        return em.find(Store.class, id);
-    }
+public interface StoreRepository extends JpaRepository<Store, Long> {
 
 }
